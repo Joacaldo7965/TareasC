8ca8227bc65002e269127a07dfb622aea18dff81
Joacaldo7965 <joacaldo2303@gmail.com>
2020-05-09 18:07:01 -0400

    Increible avance(falta poco)

--- tarea1.1.c
+++ tarea1.1.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
 
-    printf("xdd");
+char ** buscar_str(char ** S, int n, char * P){
+	
+    //for palabra(linea) in archivo
+    for(int i = 0;i < n; i++){
+
+   	}
+    return lmao
+}
+
+int main(){
+	//Abrir archivo fp (P)
+    //Abrir archivo fs (S)
+
+    //for palabra_P in fp
+	for(int i = 0;i < k;i++){
+        los_que_estan = buscar_str(fs, len(palabra_S), palabra_P)
+
+        //Crear archivo
+        for palabra in los_que_estan:
+            archivo.write(palabra\n)
+	}		
+	buscar_str();	
 }
