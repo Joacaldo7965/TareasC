872dc7aea5c82655398bd7fa7a83d4029a0aedf7
tomasnieto <tomas.nieto@sansano.usm.cl>
2020-05-10 14:07:51 -0400

    LARGO DE ACRIVOS_S (largo)
    PERDON XD SI NO FUNCIONA

--- tarea1.1.c
+++ tarea1.1.c
@@ -3,7 +3,7 @@
 
 
 char ** buscar_str(char ** S, int n, char * P){
-	
+
     //for palabra(linea) in archivo
     for(int i = 0;i < n; i++){
 
@@ -13,15 +13,31 @@ char ** buscar_str(char ** S, int n, char * P){
 
 int main(){
 	//Abrir archivo fp (P)
+	FILE *fp;
+	fp = open("Archivo_P.txt", "w");
     //Abrir archivo fs (S)
+    FILE *fs;
+    fs = open("Archivo_S.txt", "w");
 
+    //saber cuantos strings hay en Archivo_S (n)
+    int largo=0;
+    int v=1;
+    char temp;
+    while(v){
+        temp = fread(&temp,sizeof(char),1,fs)
+        if temp == "EOF"{
+            v = 0;
+        }
+        largo++;
+    }
+    //sinceramente no se que hice arriba pero ojala funcione XDDDDD
     //for palabra_P in fp
 	for(int i = 0;i < k;i++){
-        los_que_estan = buscar_str(fs, len(palabra_S), palabra_P)
+        los_que_estan = buscar_str(fs, largo, palabra_P);
 
         //Crear archivo
         for palabra in los_que_estan:
-            archivo.write(palabra\n)
-	}		
-	buscar_str();	
+            archivo.write(palabra\n);
+	}
+	buscar_str();
 }
